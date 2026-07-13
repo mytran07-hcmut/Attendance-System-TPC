e3f942448e947145201c524c8f775a074a213343
Trần Hải My <my.tran07@hcmut.edu.vn>
2026-07-13 14:16:03 +0700

    chore: remove temporary python scripts used for code modification

--- fix_clipping.py
+++
@@ -1,8 +0,0 @@
-with open("src/app/pages/hr/schedule/schedule.html", "r") as f:
-    content = f.read()
-
-# Replace line-height-1 in the holidayName div
-content = content.replace("line-height-1 ", "")
-
-with open("src/app/pages/hr/schedule/schedule.html", "w") as f:
-    f.write(content)
